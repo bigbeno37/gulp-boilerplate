fe308416554bbd29f584c70400e21d2be0cd686c
Ben OSullivan <bigbeno37@gmail.com>
2016-02-23 02:28:28 +0000

    Added friendly message to index

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
 $(document).ready(function(){
-	
+	$("body").css({ "height": $(window).height(), "width": $(window).width() });
+	$(".centerTextBody").css({ "height": $(window).height(), "width": $(window).width() });
 });
